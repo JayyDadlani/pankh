e7b8c9ad90b5f59ea5dba2b4510be039fbf71de6
Jay Dadlani <dadlanijay123@gmail.com>
2025-05-16 19:43:18 +0530

    navigation added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='h-[200px] text-white bg-gray-600'>Navbar</div>
-  )
-}
+    <>
+      <div className="h-[100px] text-white bg-gray-600">
+        <ul className="flex gap-4">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/services">Service</Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
